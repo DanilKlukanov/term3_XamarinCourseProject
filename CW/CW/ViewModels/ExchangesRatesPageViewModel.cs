9b7c3fc82c57e115b9a5ef04d19a0be4e3c4862a
Mikhail Kozitskii <kekouke@yandex.ru>
2020-12-07 09:58:45 +1000

    Refactor code: Applying MVVM design pattern

---
+++ CW/CW/ViewModels/ExchangesRatesPageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CW.ViewModels
+{
+    public class ExchangesRatesPageViewModel : BaseViewModel
+    {
+        public StartPageViewModel StartViewModel { get; private set; }
+        public ExchangesRatesPageViewModel(StartPageViewModel startViewModel)
+        {
+            StartViewModel = startViewModel;
+        }
+    }
+}
---
+++ CW/CW/Views/Map.xaml.cs
@@ -0,0 +1,30 @@
+﻿using CW.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CW
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Map : ContentPage
+    {
+        private MapPageViewModel ViewModel { get; set; }
+        public Map()
+        {
+            InitializeComponent();           
+        }
+
+        public Map(StartPageViewModel s) : this()
+        {
+            ViewModel = new MapPageViewModel(s);
+            BindingContext = ViewModel;
+        }
+
+        protected override void OnDisappearing() => ViewModel.StartViewModel.ClosePageCommand.Execute(null);
+    }
+}
