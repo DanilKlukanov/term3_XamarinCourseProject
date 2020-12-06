81ba943caf8936e430d458242203d8108d06f56f
banysk <banysk@users.noreply.github.com>
2020-12-06 19:06:28 +1000

    Init

---
+++ CW/CW/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CW
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            NavigationPage page = new NavigationPage(new StartPage());
+            page.BarBackgroundColor = Color.FromHex("#86c5da");
+            MainPage = page;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CW/CW/StartPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CW
+{
+    public partial class StartPage : ContentPage
+    {
+        bool bMap;
+        bool bExchangeRates;
+        bool bComeIn;
+        public StartPage()
+        {
+            InitializeComponent();
+            bMap = false;
+            bExchangeRates = false;
+            bComeIn = false;
+        }
+
+        private void Map_Tapped(object sender, EventArgs e)
+        {
+            if (!bMap)
+            {
+                bMap = true;
+                Map page = new Map();
+                page.Disappearing += (s, ev) => {
+                    bMap = false;
+                };
+                Navigation.PushAsync(page);
+            }
+        }
+
+        private void ExchangeRates_Tapped(object sender, EventArgs e)
+        {
+            if (!bExchangeRates)
+            {
+                bExchangeRates = true;
+                ExchangeRates page = new ExchangeRates();
+                page.Disappearing += (s, ev) => {
+                    bExchangeRates = false;
+                };
+                Navigation.PushAsync(page);
+            }
+        }
+
+        private void ComeIn_Tapped(object sender, EventArgs e)
+        {
+            if (!bComeIn)
+            {
+                bComeIn = true;
+
+                // Navigation.PushModalAsync();
+                bComeIn = false;
+            }
+        }
+    }
+}
