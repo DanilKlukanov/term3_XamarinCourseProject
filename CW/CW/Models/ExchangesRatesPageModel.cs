d981edf579c0afd73637c7967a604f5f3e461fe1
master <58977579+alexmasterblack@users.noreply.github.com>
2020-12-22 16:48:39 +1000

    Add ExchangeRates

---
+++ CW/CW/Models/ExchangesRatesPageModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CW.Models
+{
+    public class ExchangesRatesPageModel
+    {
+        public string ID { get; set; }
+        public double NumCode { get; set; }
+        public string CharCode { get; set; }
+        public short Nominal { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public decimal Previous { get; set; }
+        public decimal Buy { get; set; }
+        public decimal Sell { get; set; }
+        public decimal PreviousBuy { get; set; }
+        public decimal PreviousSell { get; set; }
+        public string Flag { get; set; }
+    }
+}
--- CW/CW/ViewModels/ExchangesRatesPageViewModel.cs
+++ CW/CW/ViewModels/ExchangesRatesPageViewModel.cs
@@ -1,15 +1,70 @@
-﻿using System;
+﻿using CW.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Windows.Input;
+using System.Xml;
+using Xamarin.Forms;
 
 namespace CW.ViewModels
 {
     public class ExchangesRatesPageViewModel : BaseViewModel
     {
         public StartPageViewModel StartViewModel { get; private set; }
+        public ObservableCollection<ExchangesRatesPageModel> Rates { get; private set; }
+        public string CurrentDate { get; private set; }
+        private List<string> FlagCode = new List<string>
+        {
+            "🇦🇺",
+            "🇦🇿",
+            "🇬🇧",
+            "🇦🇲",
+        };
         public ExchangesRatesPageViewModel(StartPageViewModel startViewModel)
         {
             StartViewModel = startViewModel;
+            Rates = new ObservableCollection<ExchangesRatesPageModel>();
+            LoadRates();
+        }
+        async private void LoadRates()
+        {
+            CurrentDate = DateTime.Today.ToString("d");
+            string url = "https://www.cbr-xml-daily.ru/daily_json.js";
+            try
+            {
+                HttpClient client = new HttpClient();
+                client.BaseAddress = new Uri(url);
+                var response = await client.GetAsync(client.BaseAddress);
+                response.EnsureSuccessStatusCode();
+                var content = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(content);
+                JObject valutes = json["Valute"].Value<JObject>();
+                foreach (KeyValuePair<string, JToken> sign in valutes)
+                {
+                    if (valutes[sign.Key]["Nominal"].ToString() == "1")
+                    {
+                        var about = sign.Value.ToString();
+                        var clear = Decimal.Parse(valutes[sign.Key]["Value"].ToString());
+                        var previousClear = Decimal.Parse(valutes[sign.Key]["Previous"].ToString());
+                        valutes[sign.Key]["Buy"] = Math.Round(clear + (clear * 0.025m), 2, MidpointRounding.AwayFromZero).ToString();
+                        valutes[sign.Key]["Sell"] = Math.Round(clear + (clear * -0.012m), 2, MidpointRounding.AwayFromZero).ToString();
+                        valutes[sign.Key]["PreviousBuy"] = Math.Round(previousClear + (previousClear * 0.025m), 2, MidpointRounding.AwayFromZero).ToString();
+                        valutes[sign.Key]["PreviousSell"] = Math.Round(previousClear + (previousClear * -0.012m), 2, MidpointRounding.AwayFromZero).ToString();
+                        Rates.Add(JsonConvert.DeserializeObject<ExchangesRatesPageModel>(sign.Value.ToString()));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                StartViewModel.ClosePageCommand.Execute(null);
+                await Application.Current.MainPage.DisplayAlert("Error", ex.Message.ToString(), "Оk");
+            }
         }
     }
 }
