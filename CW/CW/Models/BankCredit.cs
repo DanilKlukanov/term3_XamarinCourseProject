dc9a462e14f52e1f89d056e75d3104ca1c606441
Mary <ysters14@gmail.com>
2021-01-06 15:12:01 +1000

    Modify view in main screen
    Modify view in main screen

--- CW/CW/Models/BankCredit.cs
+++ CW/CW/Models/BankCredit.cs
@@ -7,13 +7,6 @@ namespace CW.Models
     public class BankCredit : BankItem
     {
         public DateTime Date { get; set; }
-<<<<<<< HEAD
-<<<<<<< HEAD
         public string PaymentInfo { get; set; }
-=======
->>>>>>> dc9f924... Add models for MainScreen, group of bank items
-=======
-        public string PaymentInfo { get; set; }
->>>>>>> 3f70ec5... Work on MainScreenView
     }
 }
