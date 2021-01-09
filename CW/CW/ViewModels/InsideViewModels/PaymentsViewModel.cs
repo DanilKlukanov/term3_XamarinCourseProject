e086ee11bb4e44843de965e29fe9ed9790d73db4
Qivikavi <61878419+Qivikavi@users.noreply.github.com>
2021-01-09 18:24:23 +1000

    Payments front

--- CW/CW/ViewModels/InsideViewModels/PaymentsViewModel.cs
+++ CW/CW/ViewModels/InsideViewModels/PaymentsViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using CW.Views.InsideViews;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
+using CW.Models;
+using System.Collections.ObjectModel;
 
 namespace CW.ViewModels.InsideViewModels
 {
-    class PaymentsViewModel : BaseViewModel
+    public class PaymentsViewModel : BaseViewModel
     {
         private bool _isEnabled;
 
@@ -15,16 +18,57 @@ namespace CW.ViewModels.InsideViewModels
             Navigation = navigation;
             _isEnabled = true;
 
+            ListTamplates();
+
+            OpenProfilePageCommand = new Command(OpenProfilePage);
             BackCommand = new Command(Back, () => _isEnabled);
         }
 
+        public ObservableCollection<NameOperationInTemplate> PayTemplates { get; private set; }
+
         public INavigation Navigation { get; private set; }
         public ICommand BackCommand { get; private set; }
-
+        public ICommand OpenProfilePageCommand { get; private set; }
 
         private void Back()
         {
             Navigation.PopAsync();
+        }
+
+        private void ListTamplates()
+        {
+            PayTemplates = new ObservableCollection<NameOperationInTemplate>
+            {
+                new NameOperationInTemplate
+                {
+                    Name = "Перевод между своими счетами и картами",
+                    NameOperation = "Перевод",
+                    Money = 1
+                },
+                new NameOperationInTemplate
+                {
+                    Name = "Данил Вадимович У.",
+                    NameOperation = "Перевод",
+                    Money = 1800
+                },
+                new NameOperationInTemplate
+                {
+                    Name = "МТС",
+                    NameOperation = "Оплата услуг",
+                    Money = 550
+                },
+                new NameOperationInTemplate
+                {
+                    Name = "Перевож между своими счетами и картами",
+                    NameOperation = "Пополнение баланса",
+                    Money = 150
+                },
+            };
+        }
+
+        private void OpenProfilePage()
+        {
+            Navigation.PushAsync(new ProfileView(new ProfileViewModel(Navigation)));
         }
     }
 }
