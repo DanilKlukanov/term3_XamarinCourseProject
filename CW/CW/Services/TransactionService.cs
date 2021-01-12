6d4175191db274583eb44f0f188fc26566b54b5c
master <58977579+alexmasterblack@users.noreply.github.com>
2021-01-12 18:28:59 +1000

    Add transaction for PaymentTopUpViewModel

---
+++ CW/CW/Services/TransactionService.cs
@@ -0,0 +1,55 @@
+﻿using CW.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW.Services
+{
+    class TransactionService
+    {
+        private readonly ClientServer _client;
+        private static TransactionService _instance;
+
+        private TransactionService()
+        {
+            _client = new ClientServer();
+        }
+        public static TransactionService Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new TransactionService();
+                }
+
+                return _instance;
+            }
+        }
+        public async Task<string> DoOperation(string number_from, string number_to, int amount)
+        {
+            try
+            {
+                string json = await _client.do_operation(number_from, number_to, amount);
+                if (json == "\"0\"")
+                {
+                    return "Операция успешно выполнена";
+                }  else if (json == "\"1\"")
+                {
+                    return "Введены неверные данные";
+                } else
+                {
+                    return "Разные валюты";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "Возникла непредвиденная ошибка. Повторите позднее.";
+            }
+
+        }
+
+    }
+}
