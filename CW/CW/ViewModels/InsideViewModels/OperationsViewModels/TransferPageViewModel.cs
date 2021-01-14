548075ba48b9060dd4b27696a9e13a6b4e2de977
master <58977579+alexmasterblack@users.noreply.github.com>
2021-01-14 18:43:59 +1000

    Fix

--- CW/CW/ViewModels/InsideViewModels/OperationsViewModels/TransferPageViewModel.cs
+++ CW/CW/ViewModels/InsideViewModels/OperationsViewModels/TransferPageViewModel.cs
@@ -16,21 +16,24 @@ namespace CW.ViewModels.InsideViewModels.OperationsViewModels
         public BankCard ToCard { get; private set; }
         public string NumberToCard { get; private set; }
         public ValidationInput Amount { get; set; }
+        public string PaymentReceiver { get; private set; }
         public ValidatableObject<string> UserPassword { get; set; }
         public ICommand ToPayCommand { get; private set; }
-        public TransferPageViewModel(BankItem fromCard, BankCard toCard)
+        public TransferPageViewModel(BankItem fromCard, BankCard toCard, string type)
         {
             FromCard = fromCard as BankCard;
             ToCard = toCard;
             NumberToCard = toCard.Number;
+            PaymentReceiver = type;
             ToPayCommand = new Command(ToPay);
             Amount = new ValidationInput();
             UserPassword = new ValidatableObject<string>();
         }
-        public TransferPageViewModel(BankItem fromCard, string numberCard)
+        public TransferPageViewModel(BankItem fromCard, string numberCard, string type)
         {
             FromCard = fromCard as BankCard;
             NumberToCard = numberCard;
+            PaymentReceiver = type;
             ToPayCommand = new Command(ToPay);
             Amount = new ValidationInput();
             UserPassword = new ValidatableObject<string>();
