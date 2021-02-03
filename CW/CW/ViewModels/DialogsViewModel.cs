e2c81ab419d3ad23889bb31b63397c2e60acc657
Mikhail Kozitskii <kekouke@yandex.ru>
2021-02-03 16:26:09 +1000

    Add validations for fields of dialogs screen

--- CW/CW/ViewModels/DialogsViewModel.cs
+++ CW/CW/ViewModels/DialogsViewModel.cs
@@ -1,11 +1,14 @@
 ﻿using CW.Models;
 using CW.Services;
+using CW.Validations;
 using CW.ViewModels.InsideViewModels;
 using CW.Views.InsideViews;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Internals;
@@ -16,24 +19,33 @@ namespace CW.ViewModels
     {
         private bool _isButtonEnabled;
         public ObservableCollection<Message> Messages { get; set; }
+
         public ICommand OpenProfilePageCommand { get; private set; }
         public ICommand SendMessageCommand { get; private set; }
         public INavigation Navigation { get; private set; }
 
-        public string Recipient { get; set; }
-        public string Message { get; set; }
+
+        public ValidatableObject<string> Recipient { get; set; }
+        public ValidatableObject<string> Message { get; set; }
 
         public DialogsViewModel(INavigation navigation)
         {
             Navigation = navigation;
             _isButtonEnabled = true;
+            AddValidations();
+
             Messages = new ObservableCollection<Message>();
+
+            Navigation = navigation;
+
             SendMessageCommand = new Command(SendMessage);
             OpenProfilePageCommand = new Command(OpenProfilePage, () => IsButtonEnabled);
+            OpenProfilePageCommand = new Command(OpenProfile);
+
             GetMessages();
-            Device.StartTimer(TimeSpan.FromSeconds(15), () => { 
+            Device.StartTimer(TimeSpan.FromSeconds(15), () => {
                 GetMessages();
-                return true; 
+                return true;
             });
         }
         private bool IsButtonEnabled
@@ -56,22 +68,31 @@ namespace CW.ViewModels
             await Navigation.PushAsync(new ProfileView(new ProfileViewModel(Navigation)));
             IsButtonEnabled = true;
         }
+
+
+        private void OpenProfile(object obj)
+        {
+            Navigation.PushAsync(new ProfileView(new ProfileViewModel(Navigation)));
+        }
+
         private async void SendMessage()
         {
-            var response = await new DialogService().SendMessage(Recipient, Message);
+            if (Validate())
+            {
+                var response = await new DialogService().SendMessage(Recipient.Value, Message.Value);
 
-            if (response.IsSuccessful)
-                await Application.Current.MainPage.DisplayAlert("Уведомление", response.Value, "OK");
-            else
-                await Application.Current.MainPage.DisplayAlert("Уведомление", response.ErrorMessage, "OK");
-
+                if (response.IsSuccessful)
+                    await Application.Current.MainPage.DisplayAlert("Уведомление", response.Value, "OK");
+                else
+                    await Application.Current.MainPage.DisplayAlert("Уведомление", response.ErrorMessage, "OK");
+            }
         }
 
         private async void GetMessages()
         {
             var messages = await new DialogService().GetMessages();
-            Messages.Clear();
-            var tmp = new ObservableCollection<Message>();
+            var tmpMessages = new ObservableCollection<Message>();
+
             foreach (var item in messages)
             {
                 Color col = Color.LightGreen;
@@ -79,8 +100,7 @@ namespace CW.ViewModels
                 {
                     col = Color.LightBlue;
                 }
-
-                tmp.Add(new Message()
+                tmpMessages.Add(new Message()
                 {
                     from_ = item.from_,
                     to_ = item.to_,
@@ -89,11 +109,35 @@ namespace CW.ViewModels
                     col = col
                 });
             }
-            Messages = tmp;
-            OnPropertyChanged("Messages");          
 
-            //response.ForEach(x => Messages.Add(x));
+
+
+            Messages = tmpMessages;
+            OnPropertyChanged("Messages");
         }
+
+        private bool Validate()
+        {
+            return Recipient.Validate() && Message.Validate();
+        }
+
+
+        private void AddValidations()
+        {
+            Recipient = new ValidatableObject<string>();
+            Message = new ValidatableObject<string>();
+
+            Recipient.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "Введите имя получателя."
+            });
+            Message.Validations.Add(new IsNotNullOrEmptyRule<string>
+            {
+                ValidationMessage = "Введите текст сообщения."
+            });
+
+        }
+
 
         private void Refresh()
         {
